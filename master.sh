91e3a215f8aa11219f431497a529de593ddc2483
hfmartinez <heberthfabianmartinez@gmail.com>
2024-10-31 21:52:31 -0500

    [update] provision files

--- master.sh
+++ master.sh
@@ -2,9 +2,9 @@
 
 # Initialize Kubernetes
 echo "[TASK 1] Initialize Kubernetes Cluster"
-kubeadm init --apiserver-advertise-address=192.168.10.100 --pod-network-cidr=10.244.0.0/16 >> /root/kubeinit.log 2>/dev/null
+kubeadm init --pod-network-cidr=10.244.0.0/16
 
-# Copy Kube admin config
+# Copy Kube admin config and restart kubelet service
 echo "[TASK 2] Copy kube admin config to Vagrant user .kube directory"
 mkdir /home/vagrant/.kube
 cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
@@ -12,7 +12,7 @@ chown -R vagrant:vagrant /home/vagrant/.kube
 
 # Deploy flannel network
 echo "[TASK 3] Deploy flannel network"
-su - vagrant -c "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
 # Generate Cluster join command
 echo "[TASK 4] Generate and save cluster join command to /vagrant/joincluster.sh"
--- worker.sh
+++ worker.sh
@@ -2,7 +2,7 @@
 
 # Join worker nodes to the Kubernetes cluster
 echo "[TASK 1] Join node to Kubernetes Cluster"
-bash /vagrant/joincluster.sh 2>/dev/null
+bash /vagrant/joincluster.sh
 
 echo "===================================="
 echo 'run command: vagrant ssh master -c "kubectl get nodes -o wide"'
