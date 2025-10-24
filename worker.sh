7dec95d4b3d8f1602327bf93de0015e63ce4df66
hfmartinez <heberthfabianmartinez@gmail.com>
2025-10-24 18:38:17 -0500

    [remove] old version

--- worker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Join worker nodes to the Kubernetes cluster
-echo "[TASK 1] Join node to Kubernetes Cluster"
-bash /vagrant/joincluster.sh
-
-echo "===================================="
-echo 'run command: vagrant ssh master -c "kubectl get nodes -o wide"'
-echo "===================================="
